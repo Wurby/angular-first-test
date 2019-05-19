012e724e00c8da053ecb238899687eb787694264
Joshua Pearson <mewurby@gmail.com>
2019-05-19 00:23:14 -0600

    first few components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,27 @@
-import { NgModule } from '@angular/core';
+import { JimmyComponent } from './jimmy/jimmy.component';
+import { BubsComponent } from './bubs/bubs.component';
+import { NgModule, Component } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { componentFactoryName } from '@angular/compiler';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'bubs',
+  },
+  {
+    path: 'bubs',
+    component: BubsComponent,
+  },
+  {
+    path: 'jimmy',
+    component: JimmyComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,12 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BubsComponent } from './bubs/bubs.component';
+import { JimmyComponent } from './jimmy/jimmy.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    BubsComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, BubsComponent, JimmyComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
