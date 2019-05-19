6f6a04f616b6df497d1989c714969c587587dbe7
Joshua Pearson <mewurby@gmail.com>
2019-05-19 00:55:49 -0600

    basic menu and routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,17 @@ import { AppComponent } from './app.component';
 import { BubsComponent } from './bubs/bubs.component';
 import { JimmyComponent } from './jimmy/jimmy.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule, MatMenuModule } from '@angular/material';
 
 @NgModule({
   declarations: [AppComponent, BubsComponent, JimmyComponent],
-  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatMenuModule,
+    MatButtonModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
