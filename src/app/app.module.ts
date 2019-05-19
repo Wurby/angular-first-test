f744da50c8b7d8f8540f3557fd758f8f9e9dcf87
Joshua Pearson <mewurby@gmail.com>
2019-05-19 00:28:22 -0600

    added basic theme

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,11 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BubsComponent } from './bubs/bubs.component';
 import { JimmyComponent } from './jimmy/jimmy.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [AppComponent, BubsComponent, JimmyComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
